b213e6aebad217bad9f55c11657a7a07aca9e20e
Mu'alim Syahrido <syahridomualim@gmail.com>
2020-12-17 17:16:14 +0700

    second commit

--- app/src/main/java/com/mualim/twoactivities/MainActivity.kt
+++ app/src/main/java/com/mualim/twoactivities/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.mualim.twoactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "extra_message"
+        const val TEXT_REQUEST = 1
+        val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_send.setOnClickListener {
+            Log.d(TAG, "Button clicked")
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(EXTRA_MESSAGE, edt_message.text.toString())
+            startActivityForResult(intent, TEXT_REQUEST)
+            Toast.makeText(this, "Second Activity", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                txt_header_reply.visibility = View.VISIBLE
+                txt_message_reply.text = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                txt_message_reply.visibility = View.VISIBLE
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mualim/twoactivities/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.mualim.twoactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_REPLY = "extra_reply"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        txt_message.text = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+
+        btn_reply.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(EXTRA_REPLY, edt_message_reply.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
